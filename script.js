1c20ad46b7727f412e9e2d185d5c820c5ab43c37
yugashreshtha <77113200+yugashreshtha@users.noreply.github.com>
2026-03-12 17:29:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function reveal(){
+
+for(let i=0;i<reveals.length;i++){
+
+const windowHeight = window.innerHeight;
+const elementTop = reveals[i].getBoundingClientRect().top;
+const elementVisible = 100;
+
+if(elementTop < windowHeight - elementVisible){
+
+reveals[i].classList.add("active");
+
+}
+
+}
+
+}
+
+window.addEventListener("scroll", reveal);
